a7beaebbb15f43e881db046078af3a56f169a2a4
Alan Wong <captainalan@gmail.com>
2018-06-14 15:47:40 -0700

    Organizing OOP style

--- parse_notes.py
+++ parse_notes.py
@@ -1,5 +1,6 @@
 # Length to note value correspondences
 # Can do more maths and clean this up later
+
 len_to_val = {
     1:16,
     2:8,
@@ -17,13 +18,31 @@ class Octave:
     # Something like: 5|--d---------------d-------|
     # will have num=5, notes="--d---------------d-------"
 
+    # Constructor from string
+    def __init__(self,line):
+        self.num = line[0]
+        self.notes = line[2:-1]
+
 class Measure:
     """A measure of music in BLAH notation"""
     def __init__(self):
         self.octaves = [] # A list of notes in a given octave
 
     def addOctave(self, line):
-        pass # convert line into...
+        try:
+            self.octaves.append(Octave(line))
+        except:
+            print("Oops.")
+
+class Track:
+    """Tracks ("Songs") are composed of measures"""
+    def __init__(self):
+        self.measures = []
+
+    def addMeasure(self,measure):
+        try:
+            # check to make sure its a measure
+            self.measures.append(measure)
 
 
 # Read in file and process stuff using above classes
@@ -33,10 +52,15 @@ filename = 'megalovania.txt'
 measures = [] # A list of measure objects read in from file
 with open(filename, 'r') as score:
     for line in score:
+        currentMeasure = Measure() # Make a new measure
         if line != '\n': # While not new line
+            currentMeasure.addOctave(line)
             pass # add line to current thing
         else:
-            pass
-            # print("fart.") # Fart between each 'measure'
+            # At new line, add current measure to list of measures
+            saveMeasure = currentMeasure
+            measures.append(saveMeasure)
+            currentMeasure = Measure() # Reset measure, continue
+            
 
 
