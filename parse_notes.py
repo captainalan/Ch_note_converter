bd299dfbb3f789c73c4221c7378075c965e5e565
Alan Wong <captainalan@gmail.com>
2018-06-14 16:18:57 -0700

    Now can happily read in files as objects

--- parse_notes.py
+++ parse_notes.py
@@ -23,6 +23,9 @@ class Octave:
         self.num = line[0]
         self.notes = line[2:-1]
 
+    def __str__(self):
+        return "{0}|{1}".format(self.num,self.notes)
+
 class Measure:
     """A measure of music in BLAH notation"""
     def __init__(self):
@@ -33,34 +36,43 @@ class Measure:
             self.octaves.append(Octave(line))
         except:
             print("Oops.")
+    def __str__(self):
+        return '\n'.join([str(octave) for octave in self.octaves])
 
 class Track:
     """Tracks ("Songs") are composed of measures"""
     def __init__(self):
-        self.measures = []
+        self.measures = [] # A list of measure objects that make up a track 
+
+    def __init__(self, filename):
+        self.measures = [] # Read measures from a file
+        with open(filename, 'r') as score:
+            currentMeasure = Measure() # Make a new measure
+            for line in score:
+                if line != '\n': # While not new line
+                    currentMeasure.addOctave(line) # add line to current thing
+                else:
+                    # At new line, add current measure to list of measures
+                    saveMeasure = currentMeasure
+                    self.measures.append(saveMeasure)
+                    currentMeasure = Measure() # Reset measure, continue
 
     def addMeasure(self,measure):
         try:
-            # check to make sure its a measure
-            self.measures.append(measure)
+            self.measures.append(measure) # check to make sure its a measure
+        except:
+            print("Oops")
+
+    def __str__(self):
+        return '\n\n'.join([str(measure) for measure in self.measures])
 
 
 # Read in file and process stuff using above classes
 # --------------------------------------------------------------------
 
 filename = 'megalovania.txt'
-measures = [] # A list of measure objects read in from file
-with open(filename, 'r') as score:
-    for line in score:
-        currentMeasure = Measure() # Make a new measure
-        if line != '\n': # While not new line
-            currentMeasure.addOctave(line)
-            pass # add line to current thing
-        else:
-            # At new line, add current measure to list of measures
-            saveMeasure = currentMeasure
-            measures.append(saveMeasure)
-            currentMeasure = Measure() # Reset measure, continue
-            
+# Testing
+megalovania=Track(filename)
+print(megalovania)
 
 
