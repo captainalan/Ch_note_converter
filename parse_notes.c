5ff82006be3fb8e0c46ac287a73de47476d3796c
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2018-06-18 18:56:46 +0700

    Added C File
    Starting to make C version. Hallo from Indonesia Train! Choo choo~

---
+++ parse_notes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "octave.h"
+#include "track.h"
+#include "measure.h"
+
+
+int main(int argc, char **argv){
+//argv[0] will be myprogram.
+//argv[1] will be myfile.txt.
+//FILE *f = fopen(argv[1], "r"); // "r" for read
+
+
+
+
+
+}
+from track import Track
+
+filename = 'megalovania.txt'
+# Testing
+megalovania=Track(filename)
+
+# Get rid of measures and print everything at once
+megalovania.writeForRobots()
