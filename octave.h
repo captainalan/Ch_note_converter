73a800f1c65a23a10b8f814e5eb7ab559015751a
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2018-06-25 11:01:49 +0800

    Made new files and updates

---
+++ octave.h
@@ -0,0 +1,2 @@
+#include <stdio.h>
+#include "octave.h"
--- parse_notes.c
+++ parse_notes.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-#include "octave.h"
 #include "track.h"
-#include "measure.h"
-
 
 int main(int argc, char **argv){
 //argv[0] will be myprogram.
 //argv[1] will be myfile.txt.
-//FILE *f = fopen(argv[1], "r"); // "r" for read
 
+
+
+megalovania = Track(filename); // this needs to have a declared type
+
+// Get rid of measures and print everything at once
+megalovania.writeForRobots(); // figure out how this works in C
 
 
 
 
 }
-// from track import Track
-//
-// filename = 'megalovania.txt'
-// # Testing
-// megalovania=Track(filename)
-//
-// # Get rid of measures and print everything at once
-// megalovania.writeForRobots()
