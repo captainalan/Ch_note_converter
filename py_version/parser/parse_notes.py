18bde84ebc9cbf6508087142db8e02fab6519860
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2018-08-24 16:44:54 -0700

    Put in Folders

--- py_version/parser/parse_notes.py
+++ py_version/parser/parse_notes.py
@@ -1,13 +1,13 @@
 # Authors: Alan Wong and Gabriella Quattrone
 from track import Track
 
-filename = 'supermariotheme.txt' # Put music text file you wish to convert to Ch format here
+filename = 'reflection.txt' # Put music text file you wish to convert to Ch format here
 # Note: this program only works with the format provided on this site: https://pianoletternotes.blogspot.com/
 
 
 # You will need to edit this every time you make a new file. This is your variable name.
 # Make sure the filename is the same as this variable name when converting to chf files.
-song = 'SuperMarioBros' 
+song = 'Reflection' 
 
 # Testing
 music = Track(filename)
